ba8a86fe2fe758f08aa311457ac993270a95bb09
johncf <johncf@protonmail.com>
2018-05-23 01:07:00 +0530

    simplify parser (removed BlockParser)

--- src/parser.rs
+++ src/parser.rs
@@ -4,27 +4,15 @@ use nom::types::CompleteStr;
 use nom::{alphanumeric1, self};
 
 #[derive(Debug)]
-pub struct Block<'a> {
-    pub items: Vec<BlockItem<'a>>,
-    pub source: Option<&'a str>,
-}
-
-impl<'a> Display for Block<'a> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self.source {
-            Some(source) => write!(f, "<p source=\"{}\">", source)?,
-            None => write!(f, "<p>")?,
-        }
-        for t in &self.items {
-            write!(f, "{}", t)?;
-        }
-        write!(f, "</p>")
-    }
+pub struct Entry<'a> {
+    pub main_word: &'a str,
+    pub items: Vec<EntryItem<'a>>,
+    pub source: &'a str,
 }
 
 #[derive(Debug, PartialEq)]
-pub enum BlockItem<'a> {
-    Tagged { name: &'a str, items: Vec<BlockItem<'a>>, source: Option<&'a str> },
+pub enum EntryItem<'a> {
+    Tagged { name: &'a str, items: Vec<EntryItem<'a>>, source: Option<&'a str> },
     Comment(&'a str),
     Entity(&'a str),
     EntityBr,
@@ -35,22 +23,19 @@ pub enum BlockItem<'a> {
     UnpairedTagClose(&'a str),
 }
 
-fn write_tag_open(f: &mut Formatter, name: &str, source: Option<&str>) -> fmt::Result {
-    match source {
-        Some(source) => {
-            if name == "extra" {
-                write!(f, "<{} source=\"{}\">", name, source)
-            } else {
-                write!(f, "<{} [ERROR->]source=\"{}\">", name, source)
-            }
+impl<'a> Display for Entry<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "<entry main-word=\"{}\" source=\"{}\">", self.main_word, self.source)?;
+        for i in &self.items {
+            write!(f, "{}", i)?;
         }
-        None => write!(f, "<{}>", name),
+        write!(f, "</entry>")
     }
 }
 
-impl<'a> Display for BlockItem<'a> {
+impl<'a> Display for EntryItem<'a> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        use self::BlockItem::*;
+        use self::EntryItem::*;
         let allowed_to_dangle = &["collapse", "cs", "note", "usage"];
         match *self {
             Comment(text) => write!(f, "<--{}-->", text),
@@ -83,131 +68,57 @@ impl<'a> Display for BlockItem<'a> {
     }
 }
 
-named!(parse_items<CompleteStr, Vec<BlockItem>>, many0!(block_item));
+fn write_tag_open(f: &mut Formatter, name: &str, source: Option<&str>) -> fmt::Result {
+    match source {
+        Some(source) => {
+            if name == "p" || name == "extra" {
+                write!(f, "<{} source=\"{}\">", name, source)
+            } else {
+                write!(f, "<{} [ERROR->]source=\"{}\">", name, source)
+            }
+        }
+        None => write!(f, "<{}>", name),
+    }
+}
 
-named!(block_item<CompleteStr, BlockItem>,
+named!(parse_items<CompleteStr, Vec<EntryItem>>, many0!(entry_item));
+
+named!(entry_item<CompleteStr, EntryItem>,
        alt!(plain_text | open_tag | close_tag | entity | comment | ext_link));
 
-named!(plain_text<CompleteStr, BlockItem>,
-       map!(is_not!("<>"), |s| BlockItem::PlainText(s.0)));
+named!(plain_text<CompleteStr, EntryItem>,
+       map!(is_not!("<>"), |s| EntryItem::PlainText(s.0)));
 
 named!(source_attr<CompleteStr, CompleteStr>,
        delimited!(tag!(" source=\""), take_till!(|c| c == '"'), tag!("\"")));
 
-named!(open_tag<CompleteStr, BlockItem>,
+named!(open_tag<CompleteStr, EntryItem>,
        do_parse!(
            tag!("<") >>
            name: alphanumeric1 >>
            source: opt!(source_attr) >>
            tag!(">") >>
-           ( BlockItem::UnpairedTagOpen(name.0, source.map(|s| s.0)) )));
+           ( EntryItem::UnpairedTagOpen(name.0, source.map(|s| s.0)) )));
 
-named!(close_tag<CompleteStr, BlockItem>,
-       map!(delimited!(tag!("</"), alphanumeric1, tag!(">")), |s| BlockItem::UnpairedTagClose(s.0)));
+named!(close_tag<CompleteStr, EntryItem>,
+       map!(delimited!(tag!("</"), alphanumeric1, tag!(">")), |s| EntryItem::UnpairedTagClose(s.0)));
 
-named!(entity<CompleteStr, BlockItem>,
-       alt!(map!(tag!("<?/"), |_| BlockItem::EntityUnk) |
-            map!(tuple!(tag!("<br/"), opt!(char!('\n'))), |_| BlockItem::EntityBr) |
-            map!(delimited!(tag!("<"), alphanumeric1, tag!("/")), |s| BlockItem::Entity(s.0))));
+named!(entity<CompleteStr, EntryItem>,
+       alt!(map!(tag!("<?/"), |_| EntryItem::EntityUnk) |
+            map!(tuple!(tag!("<br/"), opt!(char!('\n'))), |_| EntryItem::EntityBr) |
+            map!(delimited!(tag!("<"), alphanumeric1, tag!("/")), |s| EntryItem::Entity(s.0))));
 
-named!(comment<CompleteStr, BlockItem>,
-       map!(delimited!(tag!("<--"), take_until!("-->"), tag!("-->")), |s| BlockItem::Comment(s.0)));
+named!(comment<CompleteStr, EntryItem>,
+       map!(delimited!(tag!("<--"), take_until!("-->"), tag!("-->")), |s| EntryItem::Comment(s.0)));
 
-named!(ext_link<CompleteStr, BlockItem>,
+named!(ext_link<CompleteStr, EntryItem>,
        do_parse!(
            tag!("<a href=\"") >>
            url: take_till!(|c| c == '"') >>
            tag!("\">") >>
            text: is_not!("<>") >>
            tag!("</a>") >>
-           ( BlockItem::ExternalLink(url.0, text.0) )));
-
-named!(block_head<CompleteStr, Option<&str>>,
-       do_parse!(
-           tag!("<p") >>
-           source: opt!(source_attr) >>
-           tag!(">") >>
-           ( source.map(|s| s.0) )));
-
-struct BlockParser<'a> {
-    contents: &'a str,
-}
-
-impl<'a> BlockParser<'a> {
-    fn new(contents: &'a str) -> BlockParser<'a> {
-        BlockParser { contents }
-    }
-}
-
-impl<'a> Iterator for BlockParser<'a> {
-    type Item = Result<Block<'a>, ParserError<'a>>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.contents.find("<p").map(|start_idx| {
-            let remaining = &self.contents[start_idx..];
-            let end_idx = match remaining.find("</p>") {
-                Some(i) => i,
-                None => {
-                    self.contents = ""; // further parsing not possible
-                    return Err(ParserError {
-                        leading: "",
-                        trailing: remaining,
-                    });
-                }
-            };
-            self.contents = &remaining[end_idx + 4..];
-            match block_head(CompleteStr(&remaining[..end_idx])) {
-                Ok((block_str, source_opt)) => {
-                    match parse_items(block_str) {
-                        Ok((unparsed, items)) => {
-                            if unparsed.len() > 0 {
-                                self.contents = ""; // further parsing not needed
-                                let lead_len = end_idx - unparsed.len();
-                                Err(ParserError {
-                                    leading: &remaining[..lead_len],
-                                    trailing: &remaining[lead_len..],
-                                })
-                            } else {
-                                Ok(Block {
-                                    items: pair_up_items(items),
-                                    source: source_opt,
-                                })
-                            }
-                        }
-                        Err(_) => unreachable!(),
-                    }
-                }
-                Err(nom::Err::Error(nom::simple_errors::Context::Code(context, _))) => {
-                    self.contents = ""; // further parsing not needed
-                    let lead_len = end_idx - context.len();
-                    Err(ParserError {
-                        leading: &remaining[..lead_len],
-                        trailing: &remaining[lead_len..],
-                    })
-                }
-                Err(_) => unreachable!(),
-            }
-        })
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct ParserError<'a> {
-    pub leading: &'a str,
-    pub trailing: &'a str,
-}
-
-impl<'a> Display for ParserError<'a> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}[ERROR->]{}", self.leading, self.trailing)
-    }
-}
-
-pub struct Entry<'a> {
-    pub main_word: &'a str,
-    pub blocks: Vec<Block<'a>>,
-    pub source: &'a str,
-}
+           ( EntryItem::ExternalLink(url.0, text.0) )));
 
 pub struct EntryParser<'a> {
     contents: &'a str,
@@ -263,21 +174,25 @@ impl<'a> Iterator for EntryParser<'a> {
             let close_len = "</entry>".len();
             self.contents = &remaining[end_idx + close_len..];
             match entry_head(&remaining[..end_idx]) {
-                Ok((block_str, EntryHead { main_word, source })) => {
-                    let mut blocks = Vec::new();
-                    for block_res in BlockParser::new(block_str) {
-                        match block_res {
-                            Ok(mut block) => blocks.push(block),
-                            Err(ParserError { trailing, .. }) => {
-                                let lead_len = end_idx - trailing.len();
-                                return Err(ParserError {
+                Ok((entry_str, EntryHead { main_word, source })) => {
+                    match parse_items(CompleteStr(entry_str)) {
+                        Ok((unparsed, items)) => {
+                            if unparsed.len() > 0 {
+                                let lead_len = end_idx - unparsed.len();
+                                Err(ParserError {
                                     leading: &remaining[..lead_len],
                                     trailing: &remaining[lead_len..end_idx + close_len],
                                 })
-                            },
+                            } else {
+                                Ok(Entry {
+                                    main_word: main_word,
+                                    items: pair_up_items(items),
+                                    source: source,
+                                })
+                            }
                         }
+                        Err(_) => unreachable!(),
                     }
-                    Ok(Entry { main_word, blocks, source })
                 }
                 Err(nom::Err::Error(nom::simple_errors::Context::Code(context, _))) => {
                     let lead_len = end_idx - context.len();
@@ -292,24 +207,26 @@ impl<'a> Iterator for EntryParser<'a> {
     }
 }
 
-impl<'a> Display for Entry<'a> {
+#[derive(Clone, Copy, Debug)]
+pub struct ParserError<'a> {
+    pub leading: &'a str,
+    pub trailing: &'a str,
+}
+
+impl<'a> Display for ParserError<'a> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "<entry main-word=\"{}\" source=\"{}\">", self.main_word, self.source)?;
-        for b in &self.blocks {
-            write!(f, "\n{}\n", b)?;
-        }
-        write!(f, "</entry>")
+        write!(f, "{}[ERROR->]{}", self.leading, self.trailing)
     }
 }
 
-fn pair_up_items<'a>(items: Vec<BlockItem<'a>>) -> Vec<BlockItem<'a>> {
-    use self::BlockItem::*;
+fn pair_up_items<'a>(items: Vec<EntryItem<'a>>) -> Vec<EntryItem<'a>> {
+    use self::EntryItem::*;
 
     let mut stack = Vec::with_capacity(items.len()*2/3 + 1);
     for item in items {
         match item {
             UnpairedTagClose(name) => {
-                let is_tag_open_name = |item: &BlockItem<'a>| {
+                let is_tag_open_name = |item: &EntryItem<'a>| {
                     match *item {
                         UnpairedTagOpen(n, src) if n == name => Some(src),
                         _ => None,
@@ -359,14 +276,14 @@ mod test {
 
     #[test]
     fn simple() {
-        let block_str = "<entry main-word=\"Q\">\n<p><hw>Q</hw> <pr>(k<umac/)</pr>, <def>the seventeenth letter of the English alphabet.</def><br/\n[<source>1913 Webster</source>]</p>\n</entry>";
-        let expected = "<entry main-word=\"Q\" source=\"1913 Webster\">\n<p><hw>Q</hw> <pr>(k<umac/)</pr>, <def>the seventeenth letter of the English alphabet.</def></p>\n</entry>";
+        let block_str = "<entry main-word=\"Q\" source=\"1913 Webster\">\n<p><hw>Q</hw> <pr>(k<umac/)</pr>, <def>the seventeenth letter of the English alphabet.</def></p>\n</entry>";
+        let expected = block_str;
         assert_eq!(expected, identity(block_str));
     }
 
     #[test]
     fn unpaired() {
-        let block_str = "<entry main-word=\"Q\">\n<p><hw>Q</hw> <def>here are two <i>unpaired tags</b>.</def></p>\n</entry>";
+        let block_str = "<entry main-word=\"Q\" source=\"\">\n<p><hw>Q</hw> <def>here are two <i>unpaired tags</b>.</def></p>\n</entry>";
         let expected = "<entry main-word=\"Q\" source=\"\">\n<p><hw>Q</hw> <def>here are two [ERROR->]<i>unpaired tags[ERROR->]</b>.</def></p>\n</entry>";
         assert_eq!(expected, identity(block_str));
     }
